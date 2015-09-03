bbbea6d8704a6bffab71deb1a0d90fcabfe6d2e7
lovexiaov <cantywei@qq.com>
2015-09-03 23:04:30 +0800

    Initial commit

---
+++ 第2章/程序输出.py
@@ -0,0 +1,13 @@
+__author__ = 'lovexiaov'
+
+#1. print only string
+print "Hello world!"
+print 'Hello world!'
+
+#2. print string with params
+print "%d, hello world, %s" % (222, "lovexiaov")
+
+
+
+
+
