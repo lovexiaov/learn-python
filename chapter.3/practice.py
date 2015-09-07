cd814e180487641e12b797bfe99e46b1a0385293
lovexiaov <cantywei@qq.com>
2015-09-07 15:49:39 +0800

    completed chapter.3's code

---
+++ chapter.3/practice.py
@@ -0,0 +1,4 @@
+__author__ = 'weixy6'
+# 1. why Python do not need declaration when create variables or functions?
+# A: 因为Python是以数据为中心，当给一个变量赋值时，先存储数据，然后再让变量指向该数据。
+# C语言则是先为数据开辟一块固定内存，然后给其设置一个唯一的id（变量名），最后再将数据存储到内存中
